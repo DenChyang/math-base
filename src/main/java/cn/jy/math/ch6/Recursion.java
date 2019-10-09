68a75382c2063b174ac12ce6214321e57ea35524
jowu <278361353@qq.com>
2019-10-09 13:58:21 +0800

    Initial commit

---
+++ src/main/java/cn/jy/math/ch6/Recursion.java
@@ -0,0 +1,12 @@
+package cn.jy.math.ch6;
+
+/**
+ * 第六章-递归
+ * @author DengQiang.Wu
+ * @create 2019-10-09 13:56
+ */
+public class Recursion {
+    public static void main(String[] args) {
+
+    }
+}
